11cf804de7bad714dc65beafc2147193b52cd601
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-05-27 12:10:48 -0400

    created backend folders with necessary files. started building out server, models/dish+user, seed/data, and db/connection files

--- README.md
+++ README.md
@@ -5,9 +5,11 @@
 [Netlify](Insert Link Here)
 
 ## Project Overview
+
 An app featuring restaurants' famous dishes from around the globe. Viewers can explore dishes by cuisine regions and types of meals (breakfast,lunch, dinner, etc). Restaurant owners will be able to create a user login and have access to add and update their featured dishes on the app. As a PMVP, Consumers and visitors to the site can also login in to add dishes to cart or add dishes to their destination list (wishlist feature).
 
 ##Schema
+
 ```
 const User = new Schema (
   {
@@ -41,16 +43,18 @@ PMVP - roles for consumer and restaraunt users
 ##Team Expectations
 [Google Doc](https://docs.google.com/document/d/1qymtHS9S8QNBLdO41zycTamGdUqOVIg1Odqpw_bscyk/edit)
 
-
 ## MVPs
+
 - Full CRUD capacity backend (Express, Mongoose, MongoDb)
 - Full CRUD capacity React frontend (detail screen, listing screen, create screen)
 - Responsive Design (desktop, mobile)
 - User Sign Up/In Authentication
 
 ## PMVPs
+
 - Landing page with carosuel
 - Shopping Cart and Wishlist Feature
 - Hamburger menu
 - User Interaction: Comments, Reviews
 - Consumer User Account and Login
+- Testing
---
+++ server.js
@@ -0,0 +1,72 @@
+import express from "express";
+import logger from "morgan";
+
+const PORT = process.env.PORT || 3000;
+
+import db from "../db/connection.js";
+import Dish from "../models/dish.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(logger("dev"));
+
+db.on("connected", () => {
+  console.log("Connected to MongoDB!");
+  app.listen(PORT, () =>
+    console.log(`Express server application is running on port ${PORT}`)
+  );
+});
+
+app.get("/", (req, res) => res.send("This is root!"));
+
+app.get("/dishes", async (req, res) => {
+  try {
+    const dishes = await Dish.find();
+    res.json(dishes);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/dishes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const dish = await Dish.findById(id);
+    if (!dish) throw Error("Dish not found");
+    res.json(dish);
+  } catch (e) {
+    console.log(e);
+    res.send("Dish not found!");
+  }
+});
+
+app.post("/dishes", async (req, res) => {
+  try {
+    const dish = await new Dish(req.body);
+    await dish.save();
+    res.status(201).json(dish);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/dishes/:id", async (req, res) => {
+  const { id } = req.params;
+  const dish = await Dish.findByIdAndUpdate(id, req.body, { new: true });
+  res.status(200).json(dish);
+});
+
+app.delete("/dishes/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Dish.findByIdAndDelete(id);
+    if (deleted) {
+      return res.status(200).send("Dish deleted");
+    }
+    throw new Error("Dish not found");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
