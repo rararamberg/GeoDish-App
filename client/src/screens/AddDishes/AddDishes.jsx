d3f7a5a32993890b1e8c1c6425ab3d1be355079e
Alejandro P Bazalar <pbzl@pbzls-MBP.local>
2021-05-28 12:39:46 -0400

    added editdish and adddishes

--- client/src/screens/AddDishes/AddDishes.jsx
+++ client/src/screens/AddDishes/AddDishes.jsx
@@ -1,9 +1,80 @@
-import Layout from "../../components/Layout/Layout";
+import { useState } from 'react'
+import Layout from '../../components/Layout/Layout'
+import { Redirect } from 'react-router-dom'
+import { createDish } from '../../services/dishes'
 
 const AddDishes = () => {
+  const [dish, setDish] = useState({
+    name: "",
+    description: "",
+    img_url: "",
+    price: "",
+  });
+
+  const [isCreated, setCreated] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setDish({
+      ...dish,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const created = await createDish(dish);
+    setCreated({ created });
+  };
+
+  if (isCreated) {
+    return <Redirect to={`/`} />;
+  }
+
   return (
     <Layout>
-      <main className="add-dishes-container">form for adding Dishes</main>
+      <main className="add-dishes-container">
+        form for adding Dishes
+        <form className="create-form" onSubmit={handleSubmit}>
+          <input
+            className="input-name"
+            placeholder="Name"
+            value={dish.name}
+            name="name"
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+          <input
+            className="input-price"
+            placeholder="Price"
+            value={dish.price}
+            name="price"
+            required
+            onChange={handleChange}
+          />
+          <textarea
+            className="textarea-description"
+            rows={10}
+            placeholder="Description"
+            value={dish.description}
+            name="description"
+            required
+            onChange={handleChange}
+          />
+          <input
+            className="input-image-link"
+            placeholder="Image Link"
+            value={dish.img_url}
+            name="imgURL"
+            required
+            onChange={handleChange}
+          />
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+      </main>
     </Layout>
   );
 };
--- client/src/screens/EditDish/EditDish.jsx
+++ client/src/screens/EditDish/EditDish.jsx
@@ -1,10 +1,103 @@
+import { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
+import { useParams, Redirect } from "react-router-dom";
+import { getDish, updateDish } from "../../services/dishes";
 
 const EditDish = () => {
+  const [dish, setDish] = useState({
+    name: "",
+    restaurant: "",
+    img_url: "",
+    restaurant_url: "",
+    description: "",
+    price: "",
+    keywords: "",
+  });
+
+  const [isUpdated, setUpdated] = useState(false);
+  let { id } = useParams();
+
+  useEffect(() => {
+    const fetchDish = async () => {
+      const dish = await getDish(id);
+      setDish(dish);
+    };
+    fetchDish();
+  }, [id]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setDish({
+      ...dish,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const updated = await updateDish(id, dish);
+    setUpdated(updated);
+  };
+
+  if (isUpdated) {
+    return <Redirect to={`/dish/${id}`} />;
+  }
+
   return (
     <Layout>
       <main className="edit-dish-container">
         you'll see the form to edit one of the dishes
+        <div className="dish-edit">
+          <div className="image-container">
+            <img
+              className="edit-dish-image"
+              src={dish.imgURL}
+              alt={dish.name}
+            />
+            <form onSubmit={handleSubmit}>
+              <input
+                className="edit-input-image-link"
+                placeholder="Image Link"
+                value={dish.imgURL}
+                name="imgURL"
+                required
+                onChange={handleChange}
+              />
+            </form>
+          </div>
+          <form className="edit-form" onSubmit={handleSubmit}>
+            <input
+              className="input-name"
+              placeholder="Name"
+              value={dish.name}
+              name="name"
+              required
+              autoFocus
+              onChange={handleChange}
+            />
+            <input
+              className="input-price"
+              placeholder="Price"
+              value={dish.price}
+              name="price"
+              required
+              onChange={handleChange}
+            />
+            <textarea
+              className="textarea-description"
+              rows={10}
+              cols={78}
+              placeholder="Description"
+              value={dish.description}
+              name="description"
+              required
+              onChange={handleChange}
+            />
+            <button type="submit" className="save-button">
+              Save
+            </button>
+          </form>
+        </div>
       </main>
     </Layout>
   );
