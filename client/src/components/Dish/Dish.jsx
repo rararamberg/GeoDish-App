b7e8539638298e796277f4a3ba4b40c1d547014b
Paul Oh <pauloh@Pauls-MBP.attlocal.net>
2021-05-28 10:29:15 -0500

    Rendering dishes to the screen,  also added CRUD functions to services/dishes

---
+++ client/src/components/Dish/Dish.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Dish = (props) => {
+  return (
+    <div>
+      <Link className="dish" to={`/dishes/${props.id}`}>
+        <img className="dish-image" src={props.img_url} alt={props.name} />
+        <div className="dish-name">{props.name}</div>
+        <div className="dish-restaurant-name">{props.restaurant}</div>
+        <div className="dish-price">{props.price}</div>
+        <div className="dish-keywords">{props.keywords}</div>
+      </Link>
+    </div>
+  );
+};
+
+export default Dish;
--- client/src/screens/Dishes/Dishes.jsx
+++ client/src/screens/Dishes/Dishes.jsx
@@ -1,9 +1,36 @@
 import Layout from "../../components/Layout/Layout";
+import Dish from "../../components/Dish/Dish";
+import { useState, useEffect } from "react";
+import { getDishes } from "../../services/dishes";
 
 const Dishes = () => {
+  const [dishes, setDishes] = useState([]);
+
+  useEffect(() => {
+    const fetchDishes = async () => {
+      const allDishes = await getDishes();
+      console.log(allDishes);
+      setDishes(allDishes);
+    };
+    fetchDishes();
+  }, []);
+
   return (
     <Layout>
-      <main className="dishes-container">This is the dishes page!</main>
+      <main className="dishes-container">
+        <span>Dish Page</span>
+        <div>
+          {dishes.map((dish) => (
+            <Dish
+              img_url={dish.img_url}
+              name={dish.name}
+              restaurant={dish.restaurant}
+              price={dish.price}
+              keywords={dish.keywords}
+            />
+          ))}
+        </div>
+      </main>
     </Layout>
   );
 };
