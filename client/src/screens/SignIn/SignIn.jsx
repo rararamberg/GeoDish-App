1d46f03f747fda586d9e55e5cbb13087657f2a46
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-05-28 10:32:47 -0400

    created form structure for sign in

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -5,6 +5,29 @@ const SignIn = () => {
     <Layout>
       <main className="sign-in-container">
         you'll see the sign in form here
+        <h2 className="log-in">LOG IN</h2>
+        <form className="login-container">
+          <label>Email Address</label>
+          <input
+            required
+            type="text"
+            name="email"
+            // value={email}
+            // onChange={handleChange}
+            placeholder="your-email@mailsite.com"
+          />
+          <label>Password</label>
+          <input
+            required
+            type="password"
+            name="password"
+            // value={password}
+            // onChange={handleChange}
+            placeholder="your-password"
+          />
+          <button className="login-btn">LOG IN</button>
+          {/* {renderError()} */}
+        </form>
       </main>
     </Layout>
   );
