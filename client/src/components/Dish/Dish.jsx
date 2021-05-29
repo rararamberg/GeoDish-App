2f3215e5d98303c2e29710255088ae4b7d81c355
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-05-29 14:17:46 -0400

    started styling and sizing for dish cards

--- client/src/components/Dish/Dish.jsx
+++ client/src/components/Dish/Dish.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
+import "./Dish.css";
 
 const Dish = (props) => {
   return (
-    <div>
+    <div className="dish-card">
       <Link className="dish" to={`/dishes/${props._id}`}>
         <img className="dish-image" src={props.img_url} alt={props.name} />
-        <div className="dish-name">{props.name}</div>
-        <div className="dish-restaurant-name">{props.restaurant}</div>
-        <div className="dish-price">{props.price}</div>
-        <div className="dish-keywords">{props.keywords}</div>
+        <div className="dish-minor-info">
+          <div className="dish-price">$ {props.price}</div>
+          <div className="dish-name">{props.name}</div>
+          <div className="dish-restaurant-name">{props.restaurant}</div>
+          <div className="dish-keywords">{props.keywords}</div>
+        </div>
       </Link>
     </div>
   );
